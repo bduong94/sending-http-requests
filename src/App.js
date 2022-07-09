2c2fab3ba0c1a11666fdd65ba787f39843e6ddb5
Brian Duong <brianminhduong@gmail.com>
2022-07-08 21:25:37 -0400

    Refactor fetchMoviesHandler to utilize async/await

--- src/App.js
+++ src/App.js
@@ -6,23 +6,20 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
 
-  function fetchMoviesHandler() {
-    fetch("https://swapi.dev/api/films/")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformedMoviesData = data.results.map((movie) => {
-          return {
-            id: movie.episode_id,
-            title: movie.title,
-            openingText: movie.opening_crawl,
-            releaseDate: movie.release_date,
-          };
-        });
+  async function fetchMoviesHandler() {
+    const response = await fetch("https://swapi.dev/api/films/");
+    const data = await response.json();
 
-        setMovies(transformedMoviesData);
-      });
+    const transformedMoviesData = data.results.map((movie) => {
+      return {
+        id: movie.episode_id,
+        title: movie.title,
+        openingText: movie.opening_crawl,
+        releaseDate: movie.release_date,
+      };
+    });
+
+    setMovies(transformedMoviesData);
   }
 
   return (
