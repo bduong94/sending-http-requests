f2743f9cd1140ba0f854f0285917a84764cc4b20
Brian Duong <brianminhduong@gmail.com>
2022-07-09 22:11:30 -0400

    Add loading state and paragraph for loading movies component

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function fetchMoviesHandler() {
+    setIsLoading(true);
     const response = await fetch("https://swapi.dev/api/films/");
     const data = await response.json();
 
@@ -20,6 +22,7 @@ function App() {
     });
 
     setMovies(transformedMoviesData);
+    setIsLoading(false);
   }
 
   return (
@@ -28,7 +31,8 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && <MoviesList movies={movies} />}
+        {isLoading && <p>Loading..</p>}
       </section>
     </React.Fragment>
   );
