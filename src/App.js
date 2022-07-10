535e223b48ba2d24ed79aa85a99fe10da387b706
Brian Duong <brianminhduong@gmail.com>
2022-07-10 11:57:01 -0400

    Add try and catch errors for fetchMoviesHandler

--- src/App.js
+++ src/App.js
@@ -11,20 +11,28 @@ function App() {
   async function fetchMoviesHandler() {
     setIsLoading(true);
     setError(null);
-    const response = await fetch("https://swapi.dev/api/films/");
-    const data = await response.json();
 
-    const transformedMoviesData = data.results.map((movie) => {
-      return {
-        id: movie.episode_id,
-        title: movie.title,
-        openingText: movie.opening_crawl,
-        releaseDate: movie.release_date,
-      };
-    });
+    try {
+      const response = await fetch("https://swapi.dev/api/films/");
+      const data = await response.json();
 
-    setMovies(transformedMoviesData);
-    setIsLoading(false);
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const transformedMoviesData = data.results.map((movie) => {
+        return {
+          id: movie.episode_id,
+          title: movie.title,
+          openingText: movie.opening_crawl,
+          releaseDate: movie.release_date,
+        };
+      });
+      setMovies(transformedMoviesData);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error.message);
+    }
   }
 
   return (
